0b6967ecdcfe2070736874b99f9bcc8e08b469f4
wooglie <imagas96@gmail.com>
2018-10-05 15:25:04 +0200

    added component

--- src/App.js
+++ src/App.js
@@ -2,27 +2,22 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-    state = { loading: false };
+import H from './components/h_component';
 
-    componentDidMount() {
-        // 测试 devServer 的代理功能
-        // fetch('/api/category')
-        //     .then(resp => resp.json())
-        //     .then(res => console.log('here here', res));
-    }
+class App extends Component {
+    state = {
+        loading: false,
+        colors: [],
+    };
+
 
     render() {
         return (
             <div className="App">
                 <div className="App-header">
                     <img src={logo} className="App-logo" alt="logo" />
-                    <h2>Welcome to React</h2>
+                    <H text="Welcome" />
                 </div>
-                <p>{ this.state.loading.toString() }</p>
-                <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
             </div>
         );
     }
---
+++ src/components/h_component.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const H = ({ text }) => (
+    <h2>{text}</h2>
+    );
+
+export default H;
